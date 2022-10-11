ee06d931bd1d90b0f72aca1bb73d428fd2320626
Seyeon <ghkdtpdus95@naver.com>
2022-10-12 02:58:33 +0900

    customized weather app

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import * as Location from 'expo-location';
 import { useEffect,useState } from 'react';
 import { StyleSheet, Text, View,ScrollView,Dimensions,
-ActivityIndicator
+ActivityIndicator,Image
 } from 'react-native';
+
+
 
 const API_KEY = '03fb0586ba74a916cb5ec0524b8235e8';
 
@@ -13,8 +15,44 @@ export default function App() {
     month:new Date().getMonth()+1,
     date:new Date().getDate()
   }
+  const [weather,setWeather] = useState({
+    Clear : "날씨 맑음",
+    Atmosphere:"대기가 좋지 않음",
+    Clouds:"흐림",
+    Snow : "눈이 와요",
+    Rain:"비가 와요",
+    Drizzle:"서리가 내려요",
+    ThunderStrom:"천둥 번개"
 
+  }); 
+
+    const [mainImg, setMainImg]=useState({
+      Clear: <Image 
+      style={styles.mainImg}
+      source={require('./img/Clear.jpg')}></Image>,
+      Clouds: <Image 
+      style={styles.mainImg}
+      source={require('./img/Clouds.jpg')}></Image>,
+      Atmosphere: <Image 
+      style={styles.mainImg}
+      source={require('./img/Atmosphere.jpg')}></Image>,
+      Snow: <Image 
+      style={styles.mainImg}
+      source={require('./img/Snow.jpg')}></Image>,
+      Rain: <Image 
+      style={styles.mainImg}
+      source={require('./img/Rain.jpg')}></Image>,
+      Drizzle: <Image 
+      style={styles.mainImg}
+      source={require('./img/Drizzle.jpg')}></Image>,
+      ThunderStrom: <Image 
+      style={styles.mainImg}
+      source={require('./img/ThunderStrom.jpg')}></Image>,
+       
+
+    });
   const [city,setCity] = useState("Loading...");
+  const [current,setCurrent] = useState([]);
   const [days,setDays] = useState([]);
   const [location,setLocation] = useState(); 
   const [ok,setOk] = useState(true);
@@ -26,15 +64,16 @@ export default function App() {
     const {coords:{latitude,longitude}}= await Location.getCurrentPositionAsync({accuracy:5});
     const location = await Location.reverseGeocodeAsync({latitude,longitude},{useGoogleMaps:false});  
     setCity(location[0].city);
-   const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units="metric"&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
     const json = await response.json();
     
-    console.log(longitude);
     // const response = await fetch(url+queryParams);
     // console.log(response+"?");
     // const json = await response.json();
     // console.log(json+"??");
-    // setDays(json.daily);
+
+    setCurrent(json);
+  
   }
   useEffect(()=>{
     ask();
@@ -50,11 +89,23 @@ export default function App() {
       showsHorizontalScrollIndicator={false} 
       horizontal  
       contentContainerStyle={styles.weather}>
-    {days==undefined?
+    {current.weather==null?
  ( <View style={styles.day}>
   <ActivityIndicator color="white" size="large"></ActivityIndicator>
 </View>) : 
-(<View></View>)  
+<>
+  <View style={styles.day}>
+    {/* <View>{mainImg[current.weather[0].main]}</View> */}
+    <Text>{"현재 "+weather[current.weather[0].main]+"!"}</Text>
+    <Text>???</Text>
+  </View>
+  <View style={styles.day}>
+    {/* <View>{mainImg[current.weather[0].main]}</View> */}
+    <Text>{"현재 "+weather[current.weather[0].main]+"!"}</Text>
+    <Text>???</Text>
+  </View>
+  </>
+  
   }
       </ScrollView>
 
@@ -64,21 +115,22 @@ export default function App() {
 
 const styles = StyleSheet.create({
 container:{
-  flex:1, backgroundColor:"teal"
+  flex:1,
+  backgroundColor:"teal"
 },
 
 city:{
-  flex:1,
   justifyContent:"center",
   alignItems:"center"
 },
 
 cityName:{
-fontSize:68,
+fontSize:50,
 fontWeight:"500"
 },
 
 weather:{
+  flex:3
 }
 ,
 day:{
@@ -92,5 +144,10 @@ fontSize:178
 desc:{
   marginTop:-30,
   fontSize:60
+},
+mainImg:{
+  width:Dimensions.get('window').width-10,
+
+  height:Dimensions.get('window').width/2
 }
 });
