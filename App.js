3b36dc7a4ca1354b1e9995a95b6a485f32af7e4d
Seyeon <ghkdtpdus95@naver.com>
2022-10-13 00:09:59 +0900

    error with accepting data value

--- App.js
+++ App.js
@@ -4,17 +4,13 @@ import { StyleSheet, Text, View,ScrollView,Dimensions,
 ActivityIndicator,Image
 } from 'react-native';
 
-
+import { Fontisto } from "@expo/vector-icons";
 
 const API_KEY = '03fb0586ba74a916cb5ec0524b8235e8';
 
 export default function App() {
 
-  const today={
-    year:new Date().getFullYear(),
-    month:new Date().getMonth()+1,
-    date:new Date().getDate()
-  }
+
   const [weather,setWeather] = useState({
     Clear : "날씨 맑음",
     Atmosphere:"대기가 좋지 않음",
@@ -22,9 +18,22 @@ export default function App() {
     Snow : "눈이 와요",
     Rain:"비가 와요",
     Drizzle:"서리가 내려요",
-    ThunderStrom:"천둥 번개"
+    ThunderStrom:"천둥 번개",
+    Mist:"안개가 꼈어요~"
 
   }); 
+
+  const icons = {
+    Clouds: "cloudy",
+    Clear: "day-sunny",
+    Atmosphere: "cloudy-gusts",
+    Snow: "snow",
+    Rain: "rains",
+    Drizzle: "rain",
+    Thunderstorm: "lightning",
+    Mist:"day-haze"
+  };
+
 
     const [mainImg, setMainImg]=useState({
       Clear: <Image 
@@ -48,15 +57,35 @@ export default function App() {
       ThunderStrom: <Image 
       style={styles.mainImg}
       source={require('./img/ThunderStrom.jpg')}></Image>,
-       
-
+      Mist:<Image 
+      style={styles.mainImg}
+      source={require('./img/Mist.jpg')}></Image>,
     });
+
+
+  
   const [city,setCity] = useState("Loading...");
   const [current,setCurrent] = useState([]);
-  const [days,setDays] = useState([]);
+  const [hourlyData,setHourlyData] = useState([]);
+  const [dayForecast,setDayForecast] = useState([]);
   const [location,setLocation] = useState(); 
   const [ok,setOk] = useState(true);
-  const ask = async() =>{
+  
+  const getDay = new Date();
+
+  var year = getDay.getFullYear();
+  var month = ('0' + (getDay.getMonth() + 1)).slice(-2);
+  var day = ('0' + getDay.getDate()).slice(-2);
+
+  
+  const today = year + '-' + month  + '-' + day;
+  const secondDay = month +'월' + (day+1)+'일';
+  const thirdDay = month +'월' + (day+1)+'일';
+  const fourthDay = month +'월' + (day+1)+'일';
+  const fifthDay = month +'월' + (day+1)+'일';
+
+
+  const setData = async() =>{
     const {granted} = await Location.requestForegroundPermissionsAsync();
   if(!granted){
     setOk(false);
@@ -64,19 +93,31 @@ export default function App() {
     const {coords:{latitude,longitude}}= await Location.getCurrentPositionAsync({accuracy:5});
     const location = await Location.reverseGeocodeAsync({latitude,longitude},{useGoogleMaps:false});  
     setCity(location[0].city);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units="metric"&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
     const json = await response.json();
-    
-    // const response = await fetch(url+queryParams);
-    // console.log(response+"?");
-    // const json = await response.json();
-    // console.log(json+"??");
-
     setCurrent(json);
   
+    const hourlyDataResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    const jsonHourlyData = await hourlyDataResponse.json();
+    setHourlyData(jsonHourlyData);
+    console.log("to Check");
+    // console.log(hourlyData.list);
+
+    hourlyData.list.map((data)=>{
+     
+      if(data.dt_txt.substr(11,20)==="12:00:00" &&
+      data.dt_txt.substr(0,10)!=today){
+        console.log(data.dt_txt);
+        setDayForecast([...dayForecast,data]);
+      }
+    })
+
   }
+
+
+
   useEffect(()=>{
-    ask();
+    setData();
   },[])
 
   return (
@@ -89,21 +130,31 @@ export default function App() {
       showsHorizontalScrollIndicator={false} 
       horizontal  
       contentContainerStyle={styles.weather}>
-    {current.weather==null?
+    {current.weather==null||dayForecast[0]==null?
  ( <View style={styles.day}>
   <ActivityIndicator color="white" size="large"></ActivityIndicator>
 </View>) : 
 <>
   <View style={styles.day}>
-    {/* <View>{mainImg[current.weather[0].main]}</View> */}
-    <Text>{"현재 "+weather[current.weather[0].main]+"!"}</Text>
-    <Text>???</Text>
+    <View>{mainImg[current.weather[0].main]}</View>
+    <Text>{"현재 "+weather[current.weather[0].main]}</Text>
+    <View>
+    <Fontisto name={icons[current.weather[0].main]} size={24} color="black" />
+    <Text>최고기온:{Math.floor(current.main.temp_max)}도, 최저기온:{Math.floor(current.main.temp_min)}도</Text>
+    </View>
   </View>
+
+  {console.log()}
   <View style={styles.day}>
-    {/* <View>{mainImg[current.weather[0].main]}</View> */}
-    <Text>{"현재 "+weather[current.weather[0].main]+"!"}</Text>
-    <Text>???</Text>
+  <View>{mainImg[dayForecast[1].weather[0].main]}</View>
+      <Text>{`${dayForecast[1].dt_txt} "${weather[dayForecast[1].weather[0].main]}"`}</Text>
+    <View>
+    <Fontisto name={icons[dayForecast[1].weather[0].main]} size={24} color="black" />
+    <Text>최고기온:{Math.floor(dayForecast[1].main.temp_max)}도, 최저기온:{Math.floor(dayForecast[1].main.temp_min)}도</Text>
+    </View>
   </View>
+
+
   </>
   
   }
@@ -120,6 +171,7 @@ container:{
 },
 
 city:{
+  flex:.3,
   justifyContent:"center",
   alignItems:"center"
 },
@@ -130,11 +182,11 @@ fontWeight:"500"
 },
 
 weather:{
-  flex:3
+justifyContent:"center"
 }
 ,
 day:{
-  width:Dimensions.get('window').width,
+  width:Dimensions.get('window').width-10,
   alignItems:"center",
 },
 temp:{
